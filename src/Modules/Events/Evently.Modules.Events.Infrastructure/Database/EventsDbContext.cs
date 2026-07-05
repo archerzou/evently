3fc620f6032458caaa23c38cfa582c22841bb7a7
Zou Zhefeng <archer.zou84@gmail.com>
2026-07-06 00:13:40 +1200

    implement category repository and event configuration

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Evently.Modules.Events.Application.Abstractions.Data;
+using Evently.Modules.Events.Domain.Categories;
 using Evently.Modules.Events.Domain.Events;
+using Evently.Modules.Events.Infrastructure.Events;
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Events.Infrastructure.Database;
@@ -7,8 +9,12 @@ namespace Evently.Modules.Events.Infrastructure.Database;
 public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options): DbContext(options), IUnitOfWork
 {
     internal DbSet<Event> Events { get; set; }
+
+    internal DbSet<Category> Categories { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Events);
+
+        modelBuilder.ApplyConfiguration(new EventConfiguration());
     }
 }

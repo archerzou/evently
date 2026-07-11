4331898ff2f6ae5b7da9c7fcabec67cca4e1edab
Zou Zhefeng <archer.zou84@gmail.com>
2026-07-12 00:17:50 +1200

    complete exception and validation

--- src/Common/Evently.Common.Application/ApplicationConfiguration.cs
+++ src/Common/Evently.Common.Application/ApplicationConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Evently.Common.Application.Behaviors;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,6 +14,12 @@ public static class ApplicationConfiguration
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssemblies(moduleAssemblies);
+
+            config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
+
+            config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
+
+            config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
         });
 
         services.AddValidatorsFromAssemblies(moduleAssemblies, includeInternalTypes: true);
